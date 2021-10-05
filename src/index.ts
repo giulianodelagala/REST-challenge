da1cfe4b1efce06480c796f1050bcc0c665976d6
Alexander <alexander.pinto@ucsp.edu.pe>
2021-10-05 10:53:16 -0500

    fix: structure

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,4 @@ import {PrismaClient} from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-async function main() {
-  const newUser = await prisma.users.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-      posts: {
-        title: 'Hello World',
-      },
-    },
-  })
-  console.log('Created new user: ', newUser)
 
-  const allUsers = await prisma.users.findMany({
-    include: {posts: true},
-  })
-  console.log('All users: ')
-  console.dir(allUsers, {depth: null})
-}
-
-main()
-  .catch(e => console.error(e))
-  .finally(async () => await prisma.disconnect())
