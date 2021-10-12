37f41e466d7cf01a2f130a3a89f7ea01a1b588f5
Victor Benavente <templario0141@gmail.com>
2021-10-12 10:51:45 -0500

    add file sendgrid

---
+++ src/api/utils/sendgrid-mail.ts
@@ -0,0 +1,23 @@
+
+require('dotenv').config();
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export const sendEmail = (email: string, code: string) => {
+  const msg = {
+    to: email,
+    from: 'victor@ravn.co', // Use the email address or domain you verified above
+    subject: 'Codigo de verificacion de correo',
+    text: `Tu codigo de verificacion es: ${code}`,
+    html: `<strong>Tu codigo de verificacion es: <span style="font-size:20px;">${code}</span></strong>`,
+  };
+  const sendMail = async () => {
+    try {
+      await sgMail.send(msg);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  sendMail();
+};
