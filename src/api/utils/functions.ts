0e6e2692a153767ea1369b5cb9e93c8eb82b9b54
Victor Benavente <templario0141@gmail.com>
2021-10-12 12:14:33 -0500

    add files: sendgrid-mail and functions, add field verifyCode in Schema User

---
+++ src/api/utils/functions.ts
@@ -0,0 +1,10 @@
+export const getCodeVerify = (): string => {
+  let result: string = '';
+  const characters: string =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength: number = characters.length;
+  for (let i = 0; i < 5; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
